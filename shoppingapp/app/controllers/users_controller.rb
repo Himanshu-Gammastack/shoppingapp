d4adcd165498de2b2e9f14a6b3fe375d12047083
Himanshu Gupta <hguptagrepruby.io>
2022-02-09 12:33:39 +0530

    completed loginSignup

---
+++ shoppingapp/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User account created'
+      redirect_to '/'
+    else
+      flash[:warning] = 'failed account creation try again'
+      redirect_to 'users/new'
+    end
+  end
+
+
+  private
+  def user_params
+    puts "params"
+    params.require(:user).permit(:name, :email_id, :contact_number, :password, :password_confirmation)
+  end
+
+end
---
+++ shoppingapp/db/migrate/20220207174224_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email_id, unique: true
+      t.string :contact_number
+      t.string :password
+      t.timestamps
+    end
+  end
+end
